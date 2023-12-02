4d29817919249f5430b8184a04bbbbdf5f355aa0
John Bettencourt <johnnybettencourt75@gmail.com>
2023-12-02 14:03:56 -0700

    made my calendarSlice and added mock data to the calendar.

--- src/features/Calendar/Calendar.js
+++ src/features/Calendar/Calendar.js
@@ -2,27 +2,34 @@ import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import listPlugin from '@fullcalendar/list';
+import { useSelector } from 'react-redux';
+import { selectEvents } from './calendarSlice';
 import './calendar.css';
 
 export default function Calendar() {
+    // Fetch events from Redux store
+    const events = useSelector(selectEvents);
+
     return (
-        <div className="flex m-4 font-sans">
+        <div className="flex m-4">
             <div className='w-3/4 h-3/4 m-1'>
                 <div className='p-4 bg-white rounded-lg shadow'>
                     <FullCalendar
                         plugins={[dayGridPlugin]}
                         initialView='dayGridMonth'
+                        events={events} // Pass the events to FullCalendar
                     />
                 </div>
             </div>
             <div className='w-1/4 h-full m-1'>
                 <div className='p-4 bg-white rounded-lg shadow'>
-                <FullCalendar
+                    <FullCalendar
                         plugins={[listPlugin]}
                         initialView='listWeek'
+                        events={events} // Pass the events to FullCalendar
                     />
                 </div>
             </div>
         </div>
-    )
+    );
 }
---
+++ src/features/Calendar/calendarSlice.js
@@ -0,0 +1,65 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    events: [
+        {
+            id: 1,
+            title: 'Family Brunch',
+            start: '2023-12-01T10:30:00',
+            end: '2023-12-01T12:00:00',
+            allDay: false
+        },
+        {
+            id: 2,
+            title: 'Doctor Appointment',
+            start: '2023-12-03',
+            allDay: true
+        },
+        {
+            id: 3,
+            title: 'School Play',
+            start: '2023-12-05T18:00:00',
+            end: '2023-12-05T20:00:00',
+            allDay: false
+        },
+        {
+            id: 4,
+            title: 'Grocery Shopping',
+            start: '2023-12-07T09:00:00',
+            end: '2023-12-07T10:00:00',
+            allDay: false
+        },
+        {
+            id: 5,
+            title: 'Weekend Getaway',
+            start: '2023-12-10',
+            end: '2023-12-12',
+            allDay: true
+        }
+    ],
+    // nextId: 6,
+};
+
+const calendarSlice = createSlice({
+    name: 'calendar',
+    initialState,
+    reducers: {
+        addEvent: (state, action) => {
+            const newId = state.nextId++;
+
+            // Create a new task object based on the action payload
+            const newEvent = {
+                ...action.payload,
+                id: newId,
+            };
+            state.events.push(newEvent);
+        },
+        removeEvent: (state, action) => {
+            state.events = state.events.filter(event => event.id !== action.payload.id);
+        }
+    }
+})
+
+export const selectEvents = (state) => state.calendar.events;
+export const calendarReducer = calendarSlice.reducer;
+export const { addEvent, removeEvent } = calendarSlice.actions;
